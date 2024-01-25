80df623bb603b45deda6fbb117ba11eac5380a30
ruerii <129568954+ruerii@users.noreply.github.com>
2024-01-25 16:16:55 +0700

    Create Product.php

---
+++ no 2/Inputan Product /Product.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Product extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['name', 'price', 'slug', 'status', 'image', 'category_id', 'active'];
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function tags(): BelongsToMany
+    {
+        return $this->belongsToMany(Tag::class);
+    }
+}
